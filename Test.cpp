0b547a795e22058ce0481d33dea7456f2d8783d2
ron_shuster <118674717+shusteron@users.noreply.github.com>
2023-04-22 22:57:07 +0300

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,131 @@
+#include "Fraction.hpp"
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include "doctest.h"
+
+TEST_CASE("zero in denominator"){
+   CHECK_THROWS(Fraction(1,0)); 
+}
+
+TEST_CASE("zero in both num and denom"){
+    CHECK_THROWS(Fraction(0,0));
+}
+
+TEST_CASE("divided with zero"){
+    Fraction a(1,2);
+
+    CHECK_THROWS(a/0.0);
+}
+
+TEST_CASE("+ operator"){
+    Fraction a(1,2), b(2,4), x(3,2), y(5,2), z(-1,2), w(-1,2),
+        k(-1,1), s(3,4), g(2,5), f(-7,4);
+
+    CHECK((a+b)==1.0);
+    CHECK((1.0+a)==x);
+    CHECK((x+1.0)==y);
+    CHECK((-1.25+s)==w);
+}
+
+TEST_CASE("- operator"){
+    Fraction a(1,2), b(2,4),c(5,3), x(1,2), y(2,3), w(-1,2),
+        d(1,0), j(17,-10), g(3,2);
+   
+    CHECK((a-b)==0.0);
+    CHECK((1.0-a)==x);
+    CHECK((c-1.0)==y);
+    CHECK((a-w)==d);
+    CHECK((-1.2-a)==j);
+    CHECK((a-(-1.0))==g);
+}
+
+TEST_CASE("* operator"){
+    Fraction a(1,2), b(2,4),c(5,3),
+     x(5,6), y(0,1) ,w(-1,2), z(-1,4), k(1,4);
+
+    CHECK((a*c)==x);
+    CHECK((0.0*a)==y);
+    CHECK((a*2.0)==b);
+    CHECK((a*w)==z);
+    CHECK((w*w)==k);
+    CHECK((a*(-1.0))==w);
+    CHECK((-1.0*w)==a);
+}
+
+TEST_CASE("/ operator"){
+    Fraction a(1,2), b(2,4),c(5,3),
+    x(3,2), y(5,9), z(-1,4);
+
+    CHECK((b/a)==1.0);
+    CHECK((2.5/c)==x);
+    CHECK((c/3.0)==y);
+    CHECK((a/(-0.5))==-1.0);
+    CHECK(((-0.5)/a)==-1.0);
+    CHECK((a/z)==-2.0);
+}
+
+TEST_CASE("++ operator"){
+    Fraction a(1,2), b(3,2), c(-1,2), x(1,2);
+
+    CHECK(++a == b);
+    CHECK(++c==x);
+    CHECK(x++ == 0.5);
+}
+
+TEST_CASE("-- operator"){
+    Fraction a(3,1), b(2,1), c(1,2);
+
+    CHECK(--a == b);
+    CHECK(--c==-0.5);
+    CHECK(a++ == 3.0);
+}
+
+TEST_CASE("< operator"){
+    Fraction a(3,1), b(2,1);
+
+    CHECK((a<b)==false);
+    CHECK((a<1.0)==false);
+    CHECK((2.0<a)==true);
+}
+
+TEST_CASE("> operator"){
+    Fraction a(2,3), b(1,3);
+
+    CHECK((a>b)==true);
+    CHECK((a>1.0)==false);
+    CHECK((2.0>a)==true);
+}
+
+TEST_CASE("<= operator"){
+    Fraction a(2,3), b(1,4), c(2,3);
+
+    CHECK((a<=c)==true);
+    CHECK((a<=1.0)==true);
+    CHECK((2.0<=a)==false);
+    CHECK((0.25<=b)==true);
+}
+
+TEST_CASE(">= operator"){
+    Fraction a(2,3), b(1,4), c(2,3);
+
+    CHECK((a>=c)==true);
+    CHECK((a>=1.0)==false);
+    CHECK((2.1>=a)==true);
+    CHECK((0.25>=b)==true);
+}
+
+TEST_CASE("== operator"){
+    Fraction a(2,3), b(1,4), c(2,3),
+        d(4,6), e(3,12);
+
+    CHECK(a==d);
+    CHECK(a==c);
+    CHECK((b==a)==false);
+    CHECK(e==0.25);
+    CHECK((0.25==b)==true);
+    CHECK((b==0.75)==false);
+}
+
+
---
+++ sources/Fraction.cpp
@@ -0,0 +1,223 @@
+#include "Fraction.hpp"
+#include <ostream>
+#include <iostream>
+
+namespace ariel{}
+using namespace std;
+
+Fraction::Fraction(){
+    this->numerator=0;
+    this->denominator=1;
+}
+
+Fraction::Fraction(int numerator, int denominator){
+    if(denominator==0){
+        throw invalid_argument("Denominator can not be zero.");
+    }
+    this->numerator=numerator;
+    this->denominator=denominator;
+}
+//------------------------------ PRIVATE METHODS --------------------------
+int Fraction::gcd(int a, int b){
+    a=abs(a);
+    b=abs(b);
+    if((a>=b)&&((a%b)==0)){
+        return b;
+    }else{
+        return gcd(b,(a%b));
+    }
+}
+
+void Fraction::reduce(){
+    int divide = this->gcd(this->getNum(), this->getDenom());
+    if(divide>1){
+        this->setNum(this->getNum()/divide);
+        this->setDenom(this->getDenom()/divide);
+    }
+}
+
+Fraction Fraction::convert(float &number){
+    if(number==0.0){
+        Fraction result(0,1);
+        return result;
+    }
+    int numerator = number*1000;
+    int denominator=1000;
+    Fraction result(numerator,denominator);
+    result.reduce();
+    return result;
+}
+
+//------------------------------ OPERATOR + ----------------------------------
+
+// Fraction + fraction.
+Fraction Fraction::operator+(const Fraction &other){
+    return Fraction();
+}
+
+// Fraction + float.
+Fraction Fraction::operator+(float number){
+    return Fraction();
+}
+
+// Float + Fraction.
+Fraction operator+(float number, const Fraction& other){
+    
+    return Fraction();
+}
+
+//------------------------------ OPERATOR "-" -------------------------------
+
+// Fraction - fraction.
+Fraction Fraction::operator-(const Fraction &other){
+   
+    return Fraction();
+}
+
+//  Fraction - float.
+Fraction Fraction::operator-(float number){
+   
+    return Fraction();
+}
+
+// Float - fraction.
+Fraction operator-(float number, const Fraction& other){
+   
+    return Fraction();
+    
+}
+
+//------------------------------ OPERATOR * -------------------------------
+
+// Fraction * fraction.
+Fraction Fraction::operator*(const Fraction &other){
+   
+    return Fraction();
+   
+}
+
+// Fraction * float.
+Fraction Fraction::operator*(float number){
+    return Fraction();
+    // return result;
+}
+
+// Float * fraction.   
+Fraction operator*(float number, const Fraction& other){
+    
+    return Fraction();
+
+}
+
+//------------------------------ OPERATOR / -------------------------------
+Fraction Fraction::operator/(const Fraction &other){
+    return Fraction();
+}
+Fraction Fraction::operator/(float number){
+    return Fraction();
+}
+Fraction operator/(float number, const Fraction& other){
+    return Fraction();
+}
+
+
+//------------------------------ OPERATOR "--" -------------------------------
+Fraction Fraction::operator--(int){
+    return Fraction();
+}
+Fraction Fraction::operator--(){
+    return Fraction();
+}
+
+//------------------------------ OPERATOR ++ -------------------------------
+//Postfix increcment.
+Fraction Fraction::operator++(int){
+    Fraction copy = *this;
+    this->numerator+=this->denominator;
+    return copy;
+    
+}
+//Pretfix increcment.
+Fraction Fraction::operator++(){
+    this->numerator+=this->denominator;
+    return *this;
+}
+
+//------------------------------ OPERATOR < -------------------------------
+bool Fraction::operator<(const Fraction &other){
+    return false;
+}
+bool Fraction::operator<(float number){
+    return false;
+}
+bool operator<(float number, const Fraction& other){
+    return false;
+}
+
+//------------------------------ OPERATOR > -------------------------------
+bool Fraction::operator>(const Fraction &other){
+    return false;
+}
+bool Fraction::operator>(float number){
+    return false;
+}
+bool operator>(float number, const Fraction& other){
+    return false;
+}
+
+
+//------------------------------ OPERATOR <= -------------------------------
+bool Fraction::operator<=(const Fraction &other){
+    return false;
+}
+bool Fraction::operator<=(float number){
+    return false;
+}
+bool operator<=(float number, const Fraction& other){
+    return false;
+}
+
+
+//------------------------------ OPERATOR >= -------------------------------
+bool Fraction::operator>=(const Fraction &other){
+    return false;
+}
+bool Fraction::operator>=(float number){
+    return false;
+}
+bool operator>=(float number, const Fraction& other){
+    return false;
+}
+
+
+//------------------------------ OPERATOR == -------------------------------
+bool Fraction::operator==(const Fraction &other) const{
+    return ((this->numerator == other.numerator)&& (this->denominator == other.denominator));
+}
+bool Fraction::operator==(float number){
+    return false;
+}
+bool operator==(float number, const Fraction& other){
+    return false;
+}
+
+
+//------------------------------ OPERATOR << -------------------------------
+ostream& operator<< (ostream& output, const Fraction& f){
+    return (output << f.numerator << '/' << f.denominator);
+}
+//------------------------------ GETTERS & SETTERS -----------------------------------
+int Fraction::getNum()const{
+    return this->numerator;
+}
+int Fraction::getDenom()const{
+    return this->denominator;
+}
+void Fraction::setNum(int num){
+    this->numerator = num;
+}
+void Fraction::setDenom(int denom){
+    this->denominator=denom;
+}
+
+
